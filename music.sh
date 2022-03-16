d16155bc1545cb9e77ee455a8c867c803211865e
Lakshay <60613697+Lakshay0124@users.noreply.github.com>
2022-03-16 16:49:51 +0000

    Update music.sh

--- music.sh
+++ music.sh
@@ -13,7 +13,7 @@ if [[ $1 == "download" ]]; then
 	printf "Song Name: "
 	read a
 	echo $a > song_needy.txt
-	sed 's/ //g' song_needy.txt > song_needy1.txt
+	sed 's/ /+/g' song_needy.txt > song_needy1.txt
 	song_req=$(cat song_needy1.txt) 
 	curl -s https://invidious.kavin.rocks/search?q=$song_req > song_qr.txt  #curl it using invidious
 	sed '240!d' song_qr.txt > song_final.txt    #all sed command filter everything and make the link to use for youtube-dl
@@ -22,17 +22,27 @@ if [[ $1 == "download" ]]; then
 	sed 's/>//' son_output.txt > song_out.txt   
 	cat song_out.txt | cut -c 32- > /dev/null 2>&1      #cut first 32 chars from the link
 	yt-dlp --extract-audio --audio-format mp3 -o "$song_req.%(ext)s.%(ext)s" $(cat song_out.txt | cut -c 32-) > /dev/null 2>&1
-	rm *.txt
+	rm *.txt	
 	printf "Success!\n"	
 	sleep 0.1
 
 	if [[ -f $song_req.webm.mp3 ]]; then
-		mv $song_req.webm.mp3 songs
+		nameyy=$(ls $song_req.webm.mp3 | sed 's/+/ /g')
+		mv $song_req.webm.mp3 songs/
+		cd songs/
+		mv "$song_req.webm.mp3" "$nameyy"
+		cd ..
+
 
 	elif [[ -f $song_req.m4a.mp3 ]]; then
-		mv $song_req.m4a.mp3 songs
-	
+		nameyy=$(ls $song_req.m4a.mp3 | sed 's/+/ /g')
+		mv $song_req.m4a.mp3 songs/
+		cd songs/
+		mv "$song_req.m4a.mp3" "$nameyy"
+		cd ..
+
 	else
+
 		printf "error 404\n"
 	
 	fi
@@ -40,9 +50,8 @@ if [[ $1 == "download" ]]; then
 
 elif [[ $1 == "play" ]]; then
 	cd songs/
-	sleep 1 
 	ls
-	echo "(just its name no extension)"
+	printf "(just its name no extension)\n"
 	printf "which song to play: "
 	read query
 
@@ -57,5 +66,5 @@ elif [[ $1 == "play" ]]; then
 	
 	fi
 
-fi 
+fi
 
