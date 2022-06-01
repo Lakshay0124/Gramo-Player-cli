0a88efe81d333d7a1a853a3c95fd7e5161228a6b
Lakshay <60613697+Lakshay0124@users.noreply.github.com>
2022-06-01 23:01:31 +0530

    Update mass-downloader.sh

--- mass-downloader.sh
+++ mass-downloader.sh
@@ -1,31 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 printf "enter name of file to download songs from: "
 read file
 no=$(wc -l "$file" | awk '{print $1}')
 i=0
 while [ $i -lt $no ]
 do
-	i=$((i+1))
+        i=$((i+1))
         name=$(sed -n $i,0p "$file")
-	song_req="$name"
+        song_req="$name"
         echo "Downloading Please Wait!"
         echo "$song_req" > req.txt
         link=$(curl -s https://vid.puffyan.us/search?q=$(sed 's/ /+/g' req.txt) | awk '/Watch/{print}' | head -n 1 | awk '{print $5}' | sed 's/href="//' | sed 's/">//')
         yt-dlp --extract-audio --audio-format mp3 -o "$song_req.%(ext)s" "$link" > /dev/null 2>&1
         printf "Success!\n"
         sleep 0.1
-        if [[ -f $song_req.mp3 ]]; then
+        if [ -f $song_req.mp3 ]; then
                 mv "$song_req.mp3" songs/
                 cd songs/
-		echo "$song_req.mp3" > name.txt
+                echo "$song_req.mp3" > name.txt
                 nameyy=$(sed s'/ /-/g' name.txt)
                 mv "$song_req.mp3" "$nameyy" > /dev/null 2>&1
                 rm *.txt
-		cd ..
+                cd ..
                                  
         
         fi
-	
+
 done
-	
+
 rm *.txt > /dev/null 2>&1
