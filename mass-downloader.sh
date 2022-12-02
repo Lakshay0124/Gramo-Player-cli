8b0fc3fdbdc9bfd769c70f556c83028b80c31c15
Lakshay <60613697+Lakshay0124@users.noreply.github.com>
2022-12-02 13:19:25 +0530

    Update mass-downloader.sh

--- mass-downloader.sh
+++ mass-downloader.sh
@@ -3,21 +3,22 @@
 download()
 {
 	#songs="$(head -n $1 songs)"
-        echo "Downloading Please Wait!"
-        echo ""$(cat songs.txt)" | head -n $1 | tail -n 1)" > "$1_req.txt"
-	 #link variable fetches link of video like fKopy74weus 
+        echo "Downloading Please Wait!" 
+	echo "$(cat "$(cat %Lks.txt)" | head -n $1 | tail -n 1)" > "$1_req.txt"
+	
+	#link variable fetches link of video like fKopy74weus 
 	
 	link=$(curl -s https://vid.puffyan.us/search?q=$(sed 's/ /+/g' "$1_req.txt") | awk '/Watch/{print}' | head -n 1 | awk '{print $5}' | sed 's/href="//' | sed 's/">//' | cut -c 33-)
 
 	audio_file="https://vid.puffyan.us/latest_version?id=$link&itag=139&local=true"
 
 		#this makes link into its audio form like https://vid.puffyan.us/embed/fKopy74weus?listen=1' 
-	curl -L -o "$(cat songs.txt | head -n "$1" | tail -n 1).m4a" "$audio_file" 
+	curl -L -o "$(cat %Lks.txt | head -n "$1" | tail -n 1).m4a" "$audio_file" 
                 
 	printf "Success!\n"
 	
 }
-no="$(wc -l "$(cat songs.txt)" | awk '{print $1}')"
+no="$(wc -l "$(cat %Lks.txt)" | awk '{print $1}')"
 i=0
 
 
@@ -27,4 +28,5 @@ do
 	download $i &
 done
 wait
-rm *.txt
+
+rm *_req.txt > /dev/null 2>&1 ; rm %Lks.txt > /dev/null 2>&1
